fe8262e138d10dea983587ec2fd71d9967ffa218
Joe Bloggs <ainenifhlanagain@hotmail.com>
2022-01-17 21:51:03 +0000

    static files

--- challenge/mysite/mysite/settings.py
+++ challenge/mysite/mysite/settings.py
@@ -145,7 +145,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
