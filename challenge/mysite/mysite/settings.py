c6841819ea37faa8e961e81038c2937937ba360f
Joe Bloggs <ainenifhlanagain@hotmail.com>
2022-01-17 22:12:02 +0000

    static for deployment

--- challenge/mysite/mysite/settings.py
+++ challenge/mysite/mysite/settings.py
@@ -15,8 +15,7 @@ import os
 #for heroku deployment
 import django_heroku
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,11 +133,13 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
